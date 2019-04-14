9722f6ad47fe66198b600e3ac417cf5e855911f9
Shuai <45868388+shuaiguoer@users.noreply.github.com>
2019-04-14 16:59:45 +0800

    Update image.py

--- image.py
+++ image.py
@@ -1,1 +1,14 @@
+import base64
 
+
+# 图片转换
+def tra(name, path, style='a+'):
+    with open(path, 'rb') as f:
+        b64str = base64.b64encode(f.read())
+    write_data = name + " = " + "%s" % b64str
+    with open('icon.py', style) as f:
+        f.write("\n"+write_data)
+
+
+tra(name='img1', path=r'E:\Python\Code\packing\windows.ico', style='w+')
+tra(name='img2', path=r'E:\Python\Code\packing\小喇叭.png')
